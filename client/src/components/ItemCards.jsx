a1bbc4fad7278f66b34a8e0347f32936a2e15aed
shreyas0924 <shreyasp0924@gmail.com>
2023-05-21 18:26:43 +0530

    product card

--- client/src/components/ItemCards.jsx
+++
@@ -1,3 +0,0 @@
-export default function ItemCards() {
-  return <div>items card</div>
-}
--- client/src/components/ShoppingList.jsx
+++ client/src/components/ShoppingList.jsx
@@ -1,18 +1,50 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import Loading from './Loading'
 
 function ShoppingList() {
-  function getData() {
-    const res = fetch('http://localhost:3000/products').then(
-      (response) => response.json
-    )
-    console.log(res)
-    return res
-  }
+  const [data, setData] = useState(null)
 
   useEffect(() => {
-    getData()
+    fetchData()
   }, [])
-  return <div></div>
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/products')
+      const jsonData = await response.json()
+      setData(jsonData)
+    } catch (error) {
+      console.log('Error fetching data:', error)
+    }
+  }
+
+  if (!data) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    )
+  }
+  return (
+    <div>
+      <section className='flex flex-wrap justify-center gap-5 m-6'>
+        {data.map((prod) => (
+          // eslint-disable-next-line react/jsx-key
+          <div className='card rounded-md overflow-hidden shadow-md bg-gray-400 text-gray-800'>
+            {/* <img src={prod.image} alt={prod.name} className='w-full h-48 object-cover' /> */}
+            <div className='p-4 text-gray-800'>
+              <h1 className='text-xl font-semibold'>{prod.name}</h1>
+              <p className=' text-sm'>₹{prod.price}</p>
+              <p className='text-sm'>Category: {prod.category}</p>
+              <button className='mt-4 bg-black text-white py-2 px-4 rounded-md hover:opacity-75 transition-opacity duration-300 ease-in-out block w-full'>
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  )
 }
 
 export default ShoppingList
