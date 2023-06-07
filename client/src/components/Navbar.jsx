99850bc0b29ddde2f5a90d9620c80ee76127fc64
shreyas0924 <shreyasp0924@gmail.com>
2023-06-07 17:25:05 +0530

    updated header

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex'>
-      <h1 className='flex-1 text-3xl text-gray-200 m-4 ml-6 '>Shopify</h1>
-
-      <Link
-        to='/cart'
-        className='text-3xl text-gray-200 text-end m-4 mr-6 cursor-pointer hover:text-blue-300'
-      >
-        Cart
-      </Link>
+    <div className='flex bg-[#131921]'>
+      <h1 className='flex-1 text-3xl text-gray-200 m-4 ml-6 '>amazon.in</h1>
+      <div className='flex '>
+        <Link
+          to='/cart'
+          className='text-3xl text-gray-200 text-end m-4 mr-6 cursor-pointer hover:text-blue-300 ml-7'
+        >
+          Cart
+        </Link>
+      </div>
     </div>
   )
 }
