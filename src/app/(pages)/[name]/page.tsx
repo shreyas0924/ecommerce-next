4b35a32f8051c49988523dcefe5d85f5b25deb0b
shreyas0924 <shreyasp0924@gmail.com>
2023-07-15 21:02:31 +0530

    slug need to be updated

--- src/app/(pages)/[name]/page.tsx
+++ src/app/(pages)/[name]/page.tsx
@@ -1,3 +1,5 @@
+import prisma from '@/lib/prisma'
+
 type ProductDescriptionProps = {
   params: { name: string }
 }
@@ -6,8 +8,19 @@ export default function ProductDescription({
   params,
 }: ProductDescriptionProps) {
   const decodedName = decodeURIComponent(params.name)
-  
+  // const product = prisma.product.findUnique({
+  //   where: {
+  //     // Update this part based on your Prisma schema
+  //     // For example, if there is an id field in ProductWhereUniqueInput:
+  //     name: decodedName,
+  //   },
+  // })
+
   return (
-    <div>Description : {decodedName ? decodedName : 'No name provided'}</div>
+    <>
+      <div className='text-2xl ml-4'>
+        Description : {decodedName ? decodedName : 'No name provided'}
+      </div>
+    </>
   )
 }
